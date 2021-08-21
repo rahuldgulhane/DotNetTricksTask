50cdc8a58c3f59a348d389bf73e24738ad06fd95
rahuldgulhane <rahuldgulhane@gmail.com>
2021-08-21 22:42:52 +0530

    push to github

---
+++ SignUPApp-master/SignUPApp/Models/CountryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignUPApp.Models
+{
+    public class CountryModel
+    {
+        public string CountryId { get; set; }
+        public string Country { get; set; }
+
+       
+    }
+    
+}
